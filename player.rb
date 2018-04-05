0dce195b120fb86b48519a7b968e6e68dea4b898
Zack Strickland <zack.a.strickland@gmail.com>
2018-04-05 16:12:22 -0400

    Add player default names

--- player.rb
+++ player.rb
@@ -2,7 +2,15 @@ class Player
 end
 
 class HumanPlayer < Player
+  attr_reader :name
+
+  def initialize(name="Dave")
+    @name = name
+  end
 end
 
 class ComputerPlayer < Player
+  def name
+    "HAL 9000"
+  end
 end
