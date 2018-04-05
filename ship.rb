6e22998702f806070b9b28d3c32de29d52154599
Zack Strickland <zack.a.strickland@gmail.com>
2018-04-05 16:54:18 -0400

    Ships can be placed on the board and checked for overlappign with other ships

--- ship.rb
+++ ship.rb
@@ -1,6 +1,33 @@
 class Ship
-  attr_reader :length
+  attr_reader :length, :coverage
   def initialize(length)
     @length = length
+    @coverage = []
+    @on_board = false
+  end
+
+  def place(x, y, horizontal)
+    unless on_board?
+      if horizontal
+        x.upto(x + (length - 1)) { |x| @coverage << [x, y] }
+      else
+        y.upto(y + (length - 1)) { |y| @coverage << [x, y] }
+      end
+      @on_board = true
+    end
+  end
+
+  def covers?(x, y)
+    @coverage.include?([x, y])
+  end
+
+  def on_board?
+    @on_board
+  end
+
+  def overlaps_with?(other_ship)
+    other_ship.coverage.any? do |coordinate|
+      covers?(coordinate[0], coordinate[1])
+    end
   end
 end
